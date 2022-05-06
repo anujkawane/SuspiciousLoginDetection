e389033b4b92efa4b23e27f9e1edb0cc2921c80d
Anuj Kawane <akawane0813@sdsu.edu>
2022-05-06 13:42:30 -0700

    Log generation code

--- ServerLogGenerator.py
+++ ServerLogGenerator.py
@@ -1,13 +1,35 @@
 from confluent_kafka import Producer, Consumer, KafkaError, KafkaException
-import socket
-import uuid
-import json
+import socket, uuid, json, random, time, pyspark.pandas as ps
+from ServerLogEnum import ServerLogEnum
 
 KAFKA_HOST = '0.0.0.0:29092'
 conf = {'bootstrap.servers': KAFKA_HOST,
         'client.id': socket.gethostname(),
-        'group.id': "FraudLoginDetection",
+        'group.id': "foo",
         'auto.offset.reset': 'smallest'}
+
+
+# random = ''
+country = ["USA", "IN", "UK", "CA", "AU", "DE", "ES", "FR", "NL", "SG", "RU", "JP", "BR", "CN", "O"]
+eventType = ["click", "purchase", "login", "log-out", "delete-account", "create-account", "update-settings", "other"]
+
+def getServerLog():
+    serverObj = ServerLogEnum(str(uuid.uuid4()),
+                              str(time.time()),
+                              str(country[random.randint(0, len(country)-1)]),
+                              str(eventType[random.randint(0, len(eventType)-1)]),
+                              str(random.randint(10000,99999)))
+
+    eventId = str(uuid.uuid4())
+    timestamp = str(time.time())
+    currentCountry  = str(country[random.randint(0, len(country)-1)])
+    currentEventType = str(eventType[random.randint(0, len(eventType)-1)])
+    accountId = str(random.randint(10000,99999))
+
+    print(serverObj.returnCommaSeparated)
+    return serverObj.returnCommaSeparated()
+
+
 
 TOPIC_SERVER_LOGS = 'server_logs'
 
@@ -19,14 +41,46 @@ def acked(err, msg):
 
 def start_producing():
     producer = Producer(conf)
-    for i in range(20):
-        message_id = str(uuid.uuid4())
 
-        producer.produce(TOPIC_SERVER_LOGS, json.dumps(i+10).encode('utf-8'), callback=acked)
+    # generate dtaftame for csv push
+    for i in range(20):
+
+        data = getServerLog()
+        # print(data)
+        # Data Generation Logic
+        # print(getServerLog())
+        # type(data)
+
+        # push to df
+        producer.produce(TOPIC_SERVER_LOGS, json.dumps(data).encode('utf-8'), callback=acked)
         producer.flush()
 
-        print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(message_id))
+        # print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(data))
         producer.poll(1)
+
+    # Push to CSV
+
+
 
 if __name__ == '__main__':
     start_producing()
+
+
+
+"""
+1. Device type checking
+2. IP address + Location checking
+3. Time stamp checking
+4. User ID 
+
+CSV File = 
+UserID
+Timestamp
+Device Type
+IP Address
+Location
+
+IP Address + Location = spark dataframe - read it from there csv
+TO generate = UserId, timestamp, device type
+exit(
+"""
